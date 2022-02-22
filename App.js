6167b30cd290cc9e6f3f50ac64736556ea9a4520
Mauricio <mauriciogamarravargas@gmail.com>
2022-02-21 22:06:33 -0400

    creacion de proyecto

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
+import React, { Component } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Top Viviendas</Text>
       <StatusBar style="auto" />
     </View>
   );
